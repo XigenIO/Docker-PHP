5e6c51f0d7050194eeb3a949c696e8b8abae604d
George Dilley <george@xigen.co.uk>
2019-01-22 09:54:24 +0000

    Updated to add pcntl and updated the build script to update the cli docker images

--- bin/build.sh
+++ bin/build.sh
@@ -11,8 +11,8 @@ source bin/utils.sh
 # dr build --squash -f cli/Dockerfile-composer -t xigen/php:cli-composer cli/
 # dr push xigen/php:cli-composer
 #
-# ./bin/build-cli.sh 73
-# dr push xigen/php:cli-73
+./bin/build-cli.sh 73
+dr push xigen/php:cli-73
 
 #########################
 #       FPM images      #
