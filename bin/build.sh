b4f224ae65f8f18d44982a12fec6d2608a9866cf
Chris Hilsdon <chrish@xigen.co.uk>
2019-01-24 11:42:16 +0000

    Remove merge tags

--- bin/build.sh
+++ bin/build.sh
@@ -5,21 +5,10 @@ source bin/utils.sh
 #########################
 #       CLI images      #
 #########################
-<<<<<<< HEAD
-# ./bin/build-cli.sh slim
-# dr push xigen/php:cli-slim
-#
-# dr build --squash -f cli/Dockerfile-composer -t xigen/php:cli-composer cli/
-# dr push xigen/php:cli-composer
-#
-./bin/build-cli.sh 73
-dr push xigen/php:cli-73
-=======
 ./bin/build-cli.sh 72
 ./bin/build-cli.sh 73
 ./bin/build-cli.sh slim
 ./bin/build-cli.sh composer
->>>>>>> Updated build scripts and removed old image files
 
 #########################
 #       FPM images      #
