dab0c40a980c2b04f3f07e2a3376eae2a4598565
Chris Hilsdon <chrish@xigen.co.uk>
2020-01-04 19:02:40 +0000

    Disable building old images

--- bin/push.sh
+++ bin/push.sh
@@ -5,12 +5,13 @@ source bin/utils.sh
 #########################
 #       CLI images      #
 #########################
-dr push xigen/php:cli-5
+#dr push xigen/php:cli-5
 dr push xigen/php:cli-72
 dr push xigen/php:cli-73
 dr push xigen/php:cli-74
 dr push xigen/php:cli-slim
 dr push xigen/php:cli-composer
+dr push xigen/php:cli-ppm
 dr push xigen/php:cli-debug
 
 dr tag xigen/php:cli-73 xigen/php:cli-7
@@ -19,8 +20,7 @@ dr push xigen/php:cli-7
 #########################
 #       FPM images      #
 #########################
-dr push xigen/php:fpm-5
-dr push xigen/php:fpm-70
+#dr push xigen/php:fpm-5
 dr push xigen/php:fpm-72
 dr push xigen/php:fpm-73
 dr push xigen/php:fpm-wordpress
