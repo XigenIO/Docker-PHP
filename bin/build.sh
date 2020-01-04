afa41f765d83f68c2fed4f0561e8abe0e97daecb
Chris Hilsdon <chrish@xigen.co.uk>
2020-01-04 18:57:58 +0000

    Disable building old images

--- bin/build.sh
+++ bin/build.sh
@@ -15,17 +15,17 @@ fi
 source bin/utils.sh
 
 
-if [ $build_debug_enable = 1 ]; then
-      echo "##################################################"
-      echo "#############       EDGE images      #############"
-      echo "##################################################"
-      ./bin/build-edge.sh
-fi
+#if [ $build_debug_enable = 1 ]; then
+#      echo "##################################################"
+#      echo "#############       EDGE images      #############"
+#      echo "##################################################"
+#      ./bin/build-edge.sh
+#fi
 
 echo "##################################################"
 echo "##############       CLI images      #############"
 echo "##################################################"
-./bin/build-cli.sh 5
+#./bin/build-cli.sh 5
 ./bin/build-cli.sh 72
 ./bin/build-cli.sh 73
 
@@ -37,7 +37,7 @@ echo "##################################################"
 echo "##################################################"
 echo "##############       FPM images      #############"
 echo "##################################################"
-./bin/build-fpm.sh 5
+#./bin/build-fpm.sh 5
 ./bin/build-fpm.sh 72
 ./bin/build-fpm.sh 73
 ./bin/build-fpm.sh wordpress
